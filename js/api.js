74b1da4e8358b91bb00dbf81bac0e6486d1f9340
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2025-06-20 14:53:22 -0300

    fução de buscar filmes adicionada

--- js/api.js
+++ js/api.js
@@ -1,7 +1,11 @@
-const url = 'https://tmdb-proxy.cubos-academy.workers.dev/3/discover/movie?language=pt-BR&include_adult=false'
+const urlListMovies = 'https://tmdb-proxy.cubos-academy.workers.dev/3/discover/movie?language=pt-BR&include_adult=false'
+const urlSearchMovies = "https://tmdb-proxy.cubos-academy.workers.dev/3/search/movie?language=pt-BR&include_adult=false&query="
 
-const nextBotton = document.querySelector(".btn-next")
-const prevBotton = document.querySelector('.btn-prev')
+const nextButton = document.querySelector(".btn-next")
+const prevButton = document.querySelector(".btn-prev")
+
+const input = document.querySelector(".search-input")
+const searchButton = document.querySelector(".btn-search")
 
 const movies = document.querySelector(".movies")
 
@@ -15,7 +19,7 @@ let start = 0
 
 async function API() {
     try {
-        const Response = await axios.get(url)
+        const Response = await axios.get(urlListMovies)
         return Response.data.results
     } catch (error) {
         return error
@@ -23,13 +27,13 @@ async function API() {
 }
 
 
-async function init() {
+async function initListMovie() {
     arrayMovies = await API()
     listMovies(arrayMovies)
 
 }
 
-init()
+initListMovie()
 
 
 function listMovies(array) {
@@ -67,20 +71,54 @@ function listMovies(array) {
 }
 
 
-nextBotton.addEventListener("click", () => {
+nextButton.addEventListener("click", () => {
     if(arrayMovies.length <= 6) {
         return
     }
 
+    
     start += 6
+
+    if(start >= arrayMovies.length) {
+        return
+    }
+
     listMovies(arrayMovies)
 })
 
-prevBotton.addEventListener("click", () => {
+prevButton.addEventListener("click", () => {
     if (start == 0) {
         return
     }
 
     start -= 6
     listMovies(arrayMovies)
+})
+
+
+
+async function search(filme) {
+    try {
+        const Response = await axios.get( urlSearchMovies + filme)
+        const resultado = Response.data.results
+        return resultado
+    } catch (error) {
+        return error
+    }
+}
+
+async function initSearchMovie(busca) {
+    arrayMovies = await search(busca)
+    start = 0
+    listMovies(arrayMovies)
+}
+
+searchButton.addEventListener("click", () => {
+    const filme = input.value
+
+    initSearchMovie(filme)
+
+    console.log(filme)
+
+    input.value = ""
 })
